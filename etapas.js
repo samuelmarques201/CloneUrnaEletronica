90bf45099731ec66df2105103b0b87664002d07c
Samuel Marques <samuelmaques201@gmail.com>
2021-09-21 16:44:00 -0300

    Corrigindo erro  fotosHtml

---
+++ etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+        titulo: 'GOVERNADOR', 
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '29',
+                nome: 'Cesar das Galinhas',
+                partido: 'DIF',
+                vice:'Anibal',
+                fotos:[
+                    {url:'candidato1.png', legenda: 'Governador'},
+                    {url:'candidato2.png', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+            {
+                numero: '33',
+                nome: 'Cristiane sábida',
+                partido: 'DIF',
+                vice:'Leda',
+                fotos:[
+                    {url:'candidato3.png', legenda: 'Governador'},
+                    {url:'candidato4.png', legenda: 'Vice-Governador', small: true}  
+                ] 
+            },
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE', 
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '71',
+                nome: 'Zoin do carro',
+                partido: 'DIF',
+                vice:'Marreta',
+                fotos:[
+                    {url:'candidato5.png', legenda: 'Presidente'},
+                    {url:'candidato6.png', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '65',
+                nome: 'Solange dmaia',
+                partido: 'DIF',
+                vice:'Henrique DUK',
+                fotos:[ 
+                    {url:'candidato7.png', legenda: 'Presidente'},
+                    {url:'candidato8.png', legenda: 'Vice-Preseidente', small: true}  
+                ] 
+            },
+        ]
+    }
+];
