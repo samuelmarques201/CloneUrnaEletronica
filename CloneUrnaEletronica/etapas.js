9cb0898caf9699e2549ecf150d1c7baa68f1af9a
Samuel Marques <samuelmaques201@gmail.com>
2021-09-24 00:50:21 -0300

    Limpando dados desnecessarios dos candidatos e ajuste nas fotos

--- CloneUrnaEletronica/etapas.js
+++ CloneUrnaEletronica/etapas.js
@@ -6,8 +6,7 @@ let etapas = [
             {
                 numero: '50333',
                 nome: 'Cesar das Galinhas',
-                partido: 'DOS RICOS',
-                vice:'Anibal',
+                partido: 'DOS RICOS',                
                 fotos:[
                     {url:'candidato1.png', legenda: 'Deputador Estadual'}
                 ]
@@ -16,7 +15,6 @@ let etapas = [
                 numero: '22900',
                 nome: 'Cristiane sábida',
                 partido: 'DOS POBRES',
-                vice:'Leda',
                 fotos:[
                     {url:'candidato3.png', legenda: 'Deputador Estadual'} 
                 ] 
@@ -31,7 +29,6 @@ let etapas = [
                 numero: '48',
                 nome: 'Jose das Tortas',
                 partido: 'DOS POBRES',
-                vice:'Anibal',
                 fotos:[
                     {url:'candidato2.png', legenda: 'Deputador Federal'}
                 ]
@@ -53,9 +50,8 @@ let etapas = [
         candidatos: [
             {
                 numero: '678',
-                nome: 'Palmerin trabalhador',
+                nome: 'Dr Palmerin',
                 partido: 'DOS RICOS',
-                vice:'Anibal',
                 fotos:[
                     {url:'candidato5.png', legenda: 'Senador'}                    
                 ]
@@ -64,7 +60,6 @@ let etapas = [
                 numero: '171',
                 nome: 'Carlito do luxo',
                 partido: 'DOS POBRES',
-                vice:'Leda',
                 fotos:[
                     {url:'candidato6.png', legenda: 'Senador'} 
                 ] 
@@ -79,7 +74,7 @@ let etapas = [
                 numero: '29',
                 nome: 'Klaubernilton Albertin',
                 partido: 'DOS POBRES',
-                vice:'Anibal',
+                vice:'Zé Alopin',
                 fotos:[
                     {url:'candidato7.png', legenda: 'Governador'},
                     {url:'candidato8.png', legenda: 'Vice-Governador', small: true}
@@ -89,7 +84,7 @@ let etapas = [
                 numero: '33',
                 nome: 'Jezuino dos pés',
                 partido: 'DOS RICOS',
-                vice:'Leda',
+                vice:'Construtor dos top',
                 fotos:[
                     {url:'candidato9.png', legenda: 'Governador'},
                     {url:'candidato10.png', legenda: 'Vice-Governador', small: true}  
@@ -105,7 +100,7 @@ let etapas = [
                 numero: '71',
                 nome: 'Zoin do carro',
                 partido: 'DOS RICOS',
-                vice:'Marreta',
+                vice:'Muca dos pobres',
                 fotos:[
                     {url:'candidato11.png', legenda: 'Presidente'},
                     {url:'candidato12.png', legenda: 'Vice-Presidente', small: true}
